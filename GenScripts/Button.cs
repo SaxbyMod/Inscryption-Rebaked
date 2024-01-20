4df80825ebf852b1963dfb5f725a6b9effcb64c9
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2024-01-19 19:39:31 -0500

    Update Script

--- GenScripts/Button.cs
+++ GenScripts/Button.cs
@@ -1,18 +1,25 @@
 using Godot;
 using System;
 
-public class Button : Sprite { 
-	buttons[] = new buttons[]
+public class Button : Node
+{
+	public override void _Ready()
+	{
+		// This is a way to quickly create an Array of Object
+Buttons[] buttons = new buttons[]
 {
   ButtonInstanceOne,
   ButtonInstanceTwo,
   ButtonInstancethree
 }
 
+// This is a way to create a new ButtonSliding while assigning its values, in that case we want to give it the list of button for it to do its job
 ButtonSliding slider = new ButtonSliding() 
 {
   Buttons = buttons
 }
 
+// We need to add it for the engine to call it's process method
 Addchild(slider)
+	}
 }
