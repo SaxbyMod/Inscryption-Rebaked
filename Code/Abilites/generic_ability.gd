c73db5e10da1acf9ed5920fc04b94004f4324b10
thompson937 <thomas.szepp@gmail.com>
2024-07-10 20:46:58 +0200

    Updated descriptions in generic_ability.gd

--- Code/Abilites/generic_ability.gd
+++ Code/Abilites/generic_ability.gd
@@ -1,5 +1,7 @@
 # This is the base for all card abilities
-# If a card "ticks" this code executes and depending on the turnwait
+# If a card "ticks" this code executes and depending on the turnWait
+# the turnWait variable basically means how many turns it waits until it runs
+# DO NOT CHANGE THIS, INSTEAD MAKE A DUPLICATE OF IT
 
 extends Node
 
