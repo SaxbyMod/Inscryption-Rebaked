d61f8857514cb4a432bdb67f94de5cad27a79484
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-07-11 15:23:45 -0400

    More Scale work

--- Code/card_manager.gd
+++ Code/card_manager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var damage = 5
-
+		
 func _process(delta):
-	print("Damage: ", damage)
+	if CardManager.damage <= 10:
+		print("You Win")
+	elif CardManager.damage <= 0:
+		print("You Lose")
+	print("Scale: " + str(CardManager.damage))
