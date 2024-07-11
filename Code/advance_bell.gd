6bcdbb9aaad2882d810ea3cccb635e7f5f0a9b5c
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-07-11 15:03:38 -0400

    Partially working Scale

--- Code/advance_bell.gd
+++ Code/advance_bell.gd
@@ -1,5 +1,26 @@
 extends Button
 
+var Slots = []
+var Slot = 0
+var Card = 0
+var Cards = []
 # Most braindead easy code ever
+	
 func _on_pressed():
+	for Card in get_tree().get_nodes_in_group("card"):
+		Cards.append(Card)
+	for Slot in get_tree().get_nodes_in_group("slot"):
+		Slots.append(Slot)
 	Player.advance_turn()
+	var Damage = 5
+	if !Slots[0].occupied || !Slots[1].occupied || !Slots[2].occupied || !Slots[3].occupied:
+		Damage += Cards[Card].power
+	elif !Slots[4].occupied || !Slots[5].occupied || !Slots[6].occupied || !Slots[7].occupied:
+		Damage -= Cards[Card].power
+	
+	if Damage == 10:
+		print("You Win")
+	elif Damage == 0:
+		print("You Lose")
+	
+	print("Scale: " + str(Damage))
