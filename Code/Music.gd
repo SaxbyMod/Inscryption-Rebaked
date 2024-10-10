b9662ff442ece6b3e4c8d2c2b4cae23f66a6fba4
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-10-10 15:57:42 -0400

    Music Handler Update [Not Finished]

--- Code/Music.gd
+++ Code/Music.gd
@@ -1,8 +1,15 @@
 extends Node
 
-
+var scene_id = get_tree().current_scene.name
+@export var song: AudioStream
+var current_track = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if scene_id == "loading":
+		current_track = "01.mp3"
+	elif scene_id == "deck":
+		current_track = "35.mp3"
+	song = "res://Music/" + current_track
 	pass # Replace with function body.
 
 
