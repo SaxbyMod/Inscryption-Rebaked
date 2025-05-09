5c1a17f1dfa013b22cfadcbe8dc8707df9866e5a
thompson937 <thomas.szepp@gmail.com>
2025-05-09 10:10:20 +0200

    Card data script brought to the 21st century

--- Code/card_data.gd
+++ Code/card_data.gd
@@ -4,11 +4,16 @@ class_name cardData
 @export_category("Appearance")
 @export var cardGfx : Texture2D
 @export var cardName : String = "Card Name here"
+@export var temple : String = ""
+@export var rarity : String = "common"
 @export_multiline var cardDesc: String = "Card description here"
+@export_multiline var cardLore: String = "lore here"
 
-@export_category("Stats")
-@export_enum("Blood","Bone","Energy") var costType = 0
-@export var costAmount = 0
+@export_category("Cost")
+@export var costs = {"Bone":0, "Blood":0, "Energy":0}
 
+@export_category("Abilities")
 @export var health : int = 1
 @export var power : int = 0
+@export var sigils = [""]
+@export var tribes = [""]
