94a78f7431877188b65ca46d97e653b1e6d27c57
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2025-05-16 20:51:48 -0400

    Finish card data object and fiddled a bit with scenes

--- Code/card_data.gd
+++ Code/card_data.gd
@@ -4,16 +4,22 @@ class_name cardData
 @export_category("Appearance")
 @export var cardGfx : Texture2D
 @export var cardName : String = "Card Name here"
-@export var temple : String = ""
-@export var rarity : String = "common"
+@export var temple : String = "N/A"
+@export var rarity : String = "Common"
 @export_multiline var cardDesc: String = "Card description here"
 @export_multiline var cardLore: String = "lore here"
+@export var illustrator: String = "N/A"
 
 @export_category("Cost")
-@export var costs = {"Bone":0, "Blood":0, "Energy":0}
+@export var costs = {"Bone":0, "Blood":0, "Energy":0, "Gems":[""]}
 
 @export_category("Abilities")
 @export var health : int = 1
 @export var power : int = 0
 @export var sigils = [""]
 @export var tribes = [""]
+@export var traits = [""]
+@export var secrets = [""]
+
+@export_category("Sounds")
+@export var playsound: AudioStream
