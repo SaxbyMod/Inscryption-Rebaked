502bda3dcf4f7ef76914a216bbaccf3d3633b063
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2025-07-15 15:56:51 -0400

    basic sigil bank and 2 more sigil classes

---
+++ Code/sigil_bank.gd
@@ -0,0 +1,7 @@
+class_name SigilBank
+
+var name: String
+var description: String
+var category: String # Essentially what kind of sigil is it
+var metacategorys: Array # Stores extra pools and relations the sigil can have for example the totem pool from the base game would be one.
+var icon: Texture2D
