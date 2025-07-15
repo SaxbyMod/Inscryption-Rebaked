d40c9830d065ac6565b373cb48ce8cc74f2b972d
thompson937 <thomas.szepp@gmail.com>
2025-07-15 22:52:39 +0200

    woops its not brokey anymore

--- Code/sigil_base_class.gd
+++ Code/sigil_base_class.gd
@@ -5,7 +5,7 @@ class_name Sigil
 var cardParent : Card
 
 func _ready() -> void:
-	cardParent = get_parent().get_parent().get_parent().get_parent().cardName
+	cardParent = get_parent().get_parent().get_parent().get_parent()
 
 func test():
 	print("Sigil test poke")
